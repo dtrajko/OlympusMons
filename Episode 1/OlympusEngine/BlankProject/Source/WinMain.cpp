ac8dd2ee073351e0afa0a52b469d08f14ab367bb
Dejan Trajkovic <dtrajko@gmail.com>
2021-04-19 02:09:49 +0200

    S01E01 - Creating Our First Window (complete)

--- Episode 1/OlympusEngine/BlankProject/Source/WinMain.cpp
+++ Episode 1/OlympusEngine/BlankProject/Source/WinMain.cpp
@@ -1,8 +1,24 @@
 #include <windows.h>
 
+#define MAX_NAME_STRING 256
+#define HInstance() GetModuleHandle(NULL)
 
-int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR lpCmdLine, INT nCmdShow)
+WCHAR	WindowClass[MAX_NAME_STRING];
+WCHAR	WindowTitle[MAX_NAME_STRING];
+
+INT		WindowWidth;
+INT		WindowHeight;
+
+
+int CALLBACK WinMain(HINSTANCE, HINSTANCE, LPSTR, INT) // HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, INT nCmdShow
 {
+	/* - Initialize Global Variables - */
+	wcscpy_s(WindowClass, TEXT("TutorialOneClass"));
+	wcscpy_s(WindowTitle, TEXT("Our First Window"));
+
+	WindowWidth  = 1366;
+	WindowHeight = 768;
+
 	/* - Create Window Class - */
 	WNDCLASSEX wcex;
 
@@ -14,10 +30,42 @@ int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR lpCmdLine, INT nCmdSh
 	wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
 	wcex.hbrBackground = (HBRUSH)GetStockObject(NULL_BRUSH);
 
+	wcex.hIcon = LoadIcon(0, IDI_APPLICATION);
+	wcex.hIconSm = LoadIcon(0, IDI_APPLICATION);
+
+	wcex.lpszClassName = WindowClass;
+
+	wcex.lpszMenuName = nullptr;
+
+	wcex.hInstance = HInstance();
+
+	wcex.lpfnWndProc = DefWindowProc;
+
+	RegisterClassEx(&wcex);
+
 	/* - Create and Display our Window - */
+	HWND hWnd = CreateWindow(WindowClass, WindowTitle, WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, 0, WindowWidth, WindowHeight, nullptr, nullptr, HInstance(), nullptr);
+
+	if (!hWnd) {
+		MessageBox(0, L"Failed to Create Window!", 0, 0);
+		return 0;
+	}
+
+	ShowWindow(hWnd, SW_SHOW);
 
 	/* - Listen for Message events - */
 
+	MSG msg = { 0 };
+	while (msg.message != WM_QUIT)
+	{
+		// If there are Window messages then process them.
+		if (PeekMessage(&msg, 0, 0, 0, PM_REMOVE))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+	}
 
 	return 0;
 }
